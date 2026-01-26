d912d943bbc9362a61d3dbd538101432dd6dc16f
Yumin Chu <yuminc03@gmail.com>
2026-01-26 15:07:33 +0900

    feat: PagingCardsScrollView - currentPageIndex 네이밍 수정, .simultaneousGesture로 animation 추가

--- PointerQuest.swiftpm/Scene/Main/Entity/PagingCardsScrollView.swift
+++ PointerQuest.swiftpm/Scene/Main/Entity/PagingCardsScrollView.swift
@@ -3,9 +3,10 @@ import SwiftUI
 /// 카드 스크롤 View
 struct PagingCardsScrollView: View {
   @State private var currentScrollOffset: CGFloat = 0
-  @Binding var currentPage: Int
+  @Binding var currentPageIndex: Int
   
   let cards: [Level]
+  private let scrollDampingFactor: CGFloat = 0.6
     
   /// 화면 너비
   var screenWidth: CGFloat {
@@ -46,7 +47,43 @@ struct PagingCardsScrollView: View {
     .frame(width: screenWidth)
     .offset(x: currentScrollOffset, y: 0)
     .onAppear {
-      currentScrollOffset = pagingOffset(for: currentPage)
+      currentScrollOffset = pagingOffset(for: currentPageIndex)
+    }
+    .simultaneousGesture(
+      DragGesture(minimumDistance: 1, coordinateSpace: .global)
+        .onChanged { value in
+          dragOffset = value.translation.width
+          currentScrollOffset = getCurrentScrollOffset()
+        }
+        .onEnded { value in
+          // 손가락으로 튕긴 거리(velocity) 예측값
+          let offset = value.predictedEndTranslation.width - dragOffset
+          // 너무 휙 넘어가지 않게 감속 (0.6배)
+          let velocityDiff = offset * scrollDampingFactor
+          // 현재 위치 + 관성 이동 거리를 합쳐서 최종적으로 도달할 페이지 계산
+          var newPageIndex = currentPage(for: currentScrollOffset + velocityDiff)
+          let currentItemOffset = CGFloat(currentPageIndex) * (cardWidth + cardPadding)
+          
+          if currentScrollOffset < -currentItemOffset
+             && newPageIndex == currentPageIndex
+          {
+            newPageIndex += 1
+          }
+          
+          dragOffset = 0
+          
+          // 애니메이션과 함께 해당 페이지 위치로 이동
+          withAnimation(.interpolatingSpring(
+            mass: 0.1,
+            stiffness: 20,
+            damping: 1.5,
+            initialVelocity: 0
+          )) {
+            currentPageIndex = newPageIndex
+            currentScrollOffset = getCurrentScrollOffset()
+          }
+        }
+    )
     }
   }
   
@@ -69,13 +106,15 @@ struct PagingCardsScrollView: View {
     // 대략적인 페이지 번호(실수) = (이동한 거리) / (카드 1개 크기)
     let index = logicalOffset / (cardWidth + cardPadding)
     
-    return Int(round(index))
+  /// 현재 보여주어야 할 스크롤 위치 실시간 계산
+  private func getCurrentScrollOffset() -> CGFloat {
+    return pagingOffset(for: currentPageIndex) + dragOffset
   }
 }
 
 #Preview {
   PagingCardsScrollView(
-    currentPage: .constant(0),
+    currentPageIndex: .constant(0),
     cards: LevelData.levels
   )
 }
