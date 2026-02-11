7368ce42a5db696e3d016a485ec4240f36ef4654
Yumin Chu <yuminc03@gmail.com>
2026-02-11 22:53:28 +0900

    feat: WelcomeView - 다크모드 적용을 위해, CancelButton fill(.backgorund)로 수정

--- PointerQuest.swiftpm/Scene/Welcome/WelcomeView.swift
+++ PointerQuest.swiftpm/Scene/Welcome/WelcomeView.swift
@@ -89,7 +89,7 @@ private extension WelcomeView {
           .frame(maxWidth: .infinity)
           .background(
             Capsule()
-              .fill(.white)
+              .fill(.background)
           )
       }
     }
