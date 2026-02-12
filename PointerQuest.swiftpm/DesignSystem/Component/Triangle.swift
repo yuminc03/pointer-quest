d64d7c79a8488a145babb94247465b7925a4b932
Yumin Chu <yuminc03@gmail.com>
2026-02-13 08:57:26 +0900

    feat: 화살표 촉을 화살처럼 수정

--- PointerQuest.swiftpm/DesignSystem/Component/Triangle.swift
+++ PointerQuest.swiftpm/DesignSystem/Component/Triangle.swift
@@ -4,9 +4,15 @@ struct Triangle: Shape {
   func path(in rect: CGRect) -> Path {
     var path = Path()
     
+    // Start top-left
     path.move(to: CGPoint(x: 0, y: 0))
-    path.addLine(to: CGPoint(x: rect.width, y: rect.midY))
+    // Point to the right (tip)
+    path.addLine(to: CGPoint(x: rect.width, y: rect.height / 2))
+    // Bottom-left
     path.addLine(to: CGPoint(x: 0, y: rect.height))
+    // Indent at the back to make it look like an arrowhead
+    path.addLine(to: CGPoint(x: rect.width * 0.3, y: rect.height / 2))
+    
     path.closeSubpath()
     return path
   }
